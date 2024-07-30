29a7b2fbc057735568a9198d265fcc910bfab91f
hillaryhosea <107868525+hillaryhosea@users.noreply.github.com>
2024-07-30 23:55:53 +0300

    additonal componet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Ratings from './componet/StarRatings';
+import ImageSlider from './componet/ImageSlider/ImageSlider';
+import Ratings from './componet/StarRating/StarRatings';
 
 function App() {
   return (
     <div className="App">
       <Ratings noOfStars ={10} />
+      <ImageSlider url={'picsum.photos/v2/list'} limit={'10'} page={'1'}/>
     </div>
   );
 }
